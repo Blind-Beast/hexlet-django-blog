ff197878871c3b4a6008dd4dc06ac863bb4ada9a
blindbeast <v_zemlyannikov@mail.ru>
2025-07-28 18:55:21 +0300

    apply class based views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return render(
-        request,
-        "articles/index.html",
-        context={
-            "name": "Articles",
-        },
-    )
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Articles")
