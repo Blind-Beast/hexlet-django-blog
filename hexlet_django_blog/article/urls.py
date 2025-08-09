bf2a30c83d3836556188b8b50601dfe95e2649bc
blindbeast <v_zemlyannikov@mail.ru>
2025-08-09 17:28:25 +0300

    add articles/update.html

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,5 +5,6 @@ from hexlet_django_blog.article import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="articles"),
     path("<int:id>/", views.ArticleView.as_view()),
+    path("<int:id>/edit/", views.ArticleFormEditView.as_view(), name="articles_update"),
     path("create/", views.ArticleFormCreateView.as_view(), name="articles_create"),
 ]
