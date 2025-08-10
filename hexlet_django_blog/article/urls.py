68ed707afb4209b3e03053ebe5ce93070d93c1d0
blindbeast <v_zemlyannikov@mail.ru>
2025-08-10 11:24:30 +0300

    add ability to delete articles

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,7 +4,8 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="articles"),
-    path("<int:id>/", views.ArticleView.as_view()),
     path("<int:id>/edit/", views.ArticleFormEditView.as_view(), name="articles_update"),
+    path("<int:id>/", views.ArticleView.as_view()),
     path("create/", views.ArticleFormCreateView.as_view(), name="articles_create"),
+    path("<int:id>/delete/", views.ArticleFormDeleteView.as_view(), name="articles_delete"),
 ]
